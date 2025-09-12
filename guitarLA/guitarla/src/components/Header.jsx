0e3b9ac5af803b9fa064c43724ecb80f13fbcf13
daniel <druiz@vozy.co>
2025-09-12 18:12:45 -0500

    actualización carrito

--- guitarLA/guitarla/src/components/Header.jsx
+++ guitarLA/guitarla/src/components/Header.jsx
@@ -1,6 +1,6 @@
 function Header(props) {
     const { cart } = props;
-    console.log(cart)
+    const cartTotal = () => cart.reduce((total, item) => total + (item.quantity * item.price), 0)
  
     return (
        <header className="py-5 header">
@@ -19,7 +19,10 @@ function Header(props) {
                         <img className="img-fluid" src="./public/img/carrito.png" alt="imagen carrito" />
 
                         <div id="carrito" className="bg-white p-3">
-                            <p className="text-center">El carrito esta vacio</p>
+                            {cart.length === 0 ?(
+                                <p className="text-center">El carrito esta vacio</p>
+                            ):(
+                            <>
                             <table className="w-100 table">
                                 <thead>
                                     <tr>
@@ -67,8 +70,11 @@ function Header(props) {
                                     ))}
                                 </tbody>
                             </table>
+                             <p className="text-end">Total pagar: <span className="fw-bold">${cartTotal()}</span></p>
+                            </>
+                            )}
 
-                            <p className="text-end">Total pagar: <span className="fw-bold">$899</span></p>
+                           
                             <button className="btn btn-dark w-100 mt-3 p-2">Vaciar Carrito</button>
                         </div>
                     </div>
